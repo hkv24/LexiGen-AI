58d88e7c37471fe36fd9dcc129bd40963967025d
hkv24 <harshkumarverma99@gmail.com>
2024-09-02 23:36:25 +0530

    Fixed drizzle.config.js to not expose my neon string

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:6mwDAqxp7TiY@ep-ancient-dream-a5402tcp.us-east-2.aws.neon.tech/LexiGen-AI?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
     }
 };
